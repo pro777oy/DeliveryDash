8403962990b1d93e0fdf62c3649bd7d044f6bc4d
Saad Kabir Uddin <102184777+pro777oy@users.noreply.github.com>
2026-03-13 03:28:20 +0600

    prefab added

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -2,16 +2,20 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Package"))
         {
             Debug.Log("Picked up package");
+            hasPackage = true;
+            Destroy(collision.gameObject, 0.5f);
         }
         
-        if (collision.gameObject.CompareTag("Customer"))
+        if (collision.gameObject.CompareTag("Customer") && hasPackage)
         {
             Debug.Log("Delivered package");
+            hasPackage = false;
         }
 
     }

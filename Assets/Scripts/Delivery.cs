14c2825cd4252bc9a402b1fc0616e881268028f0
Saad Kabir Uddin <102184777+pro777oy@users.noreply.github.com>
2026-03-26 19:31:14 +0600

    update getcomponent

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -3,20 +3,23 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool hasPackage;
+    [SerializeField] float destroyDelay = 0.3f; 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Package"))
+        if (collision.gameObject.CompareTag("Package") && !hasPackage)
         {
             Debug.Log("Picked up package");
             hasPackage = true;
-            Destroy(collision.gameObject, 0.5f);
+            GetComponent<ParticleSystem>().Play();
+            Destroy(collision.gameObject, destroyDelay);
         }
         
         if (collision.gameObject.CompareTag("Customer") && hasPackage)
         {
             Debug.Log("Delivered package");
             hasPackage = false;
-        }
+            GetComponent<ParticleSystem>().Stop();
+        } 
 
     }
 }
